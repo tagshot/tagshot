8577ed1aa79392ee0ca02ff21cb82ae310a1dce7
Dominik Moritz <domoritz@gmail.com>
2011-10-27 18:47:24 +0200

    backbone js fuer fotos model angefangen

--- models/foto.js
+++ models/foto.js
@@ -1,17 +1,30 @@
 $(function(){
     var foto = Backbone.Model.extend({
-        //methoden der fotoistanz als json    
+        //methoden der fotoistanz als json
+        star: function(nr,of){}
     });
 
     // array von fotos
     var Fotosklasse = Backbone.Collection.extend({
         model: foto,
         url: "http://localhost:8888/fotos.json"
-
     });
     Fotos = new Fotosklasse();
 
+    var debug = function(){
+        console.log("geladen");
+        console.log(Fotos.get(42).get("exif").date);
 
-    Fotos.bind("reset", function(){console.log("geladen")}, this);
+        //aendern
+        var foto42 = Fotos.get(42);
+        var exif = foto42.get("exif");
+        console.log(exif);
+        exif.date = "tomorrow";
+        foto42.set({"exif":exif});
+
+        foto42.save();
+    }
+
+    Fotos.bind("reset", debug, this);
     Fotos.fetch();
 });
