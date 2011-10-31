d5681bece8d099ea3bd87d4d5389596133dc8237
Dominik Moritz <domoritz@gmail.com>
2011-10-31 13:40:46 +0100

    thumbnail size slider

--- js/main.js
+++ js/main.js
@@ -17,8 +17,33 @@ $(function () {
 		text: uiSettings.searchBoxText,
 		cssClassWhenEmpty: 'search-start'
 	});
+
+	var initial_height = $("#image-view img").height();
+
+	function resize() {
+		var value = $("#thumbnail-size-slider").slider("value")/100;
+		$("#image-view img").css({height: initial_height*value});
+	}
 	
-	$(".star-me").star();
+	$("#thumbnail-size-slider").slider({
+		orientation: "horizontal",
+		range: "min",
+		min: 50, 
+		max: 300, 
+		value: 100, 
+		slide: resize,
+		change: resize
+	});
+
+	$("#date-picker input").DatePicker({
+		flat: true,
+		date: ['20011-07-28','20011-07-31'],
+		current: '2011-07-31',
+		calendars: 3,
+		mode: 'range',
+		starts: 1,
+		onchange: function(){}
+	});
 });
 
 function setCaretPosition(ctrl, pos) {
