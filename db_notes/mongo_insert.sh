aa5b60a79fb6eaa440c73b2ddc95133b124b457a
Willi <willi@jups42.de>
2011-11-04 14:35:59 +0100

    irreführende Variable umbenannt

--- db_notes/mongo_insert.sh
+++ db_notes/mongo_insert.sh
@@ -1,9 +1,9 @@
 DOC_NUM=1000
-MAX_TUPLES=20
+MAX_TAGS=20
 
-python gen.py $DOC_NUM $MAX_TUPLES > rand_data.json
+python gen.py $DOC_NUM $MAX_TAGS> rand_data.json
 
 time python mongo_insert.py rand_data.json
 
 echo "Inserted $(du -h rand_data.json) to MongoDB"
-echo "Bulk inserted $DOC_NUM documents with up to $MAX_TUPLES tuples"
+echo "Bulk inserted $DOC_NUM documents with up to $MAX_TAGS tuples"
