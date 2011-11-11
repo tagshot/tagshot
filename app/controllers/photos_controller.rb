5f73d4fb422fd7bc7c7599d2cffa049739085a27
dominik <domoritz@gmail.com>
2011-11-11 18:24:10 +0100

    ein paar mehr photos werden angezeigt

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -32,6 +32,99 @@ class PhotosController < ApplicationController
 	      "w" => 10
 	    }],
 	    :thumb => "images/photo1.jpg"
+    },{
+      :id => 45,
+	    :exif => {
+		    :date => "today",
+		    :author => "",
+		    :model => "",
+		    :bla => ""
+		  },
+	    :iptc => {
+		    :stars => {
+			    :nr => 4,
+			    :of => 5
+		    },
+		    :tags => ["hasso", "plattner", "sommer", "absolventen feier", "#public"]
+	    },
+	    :url => "",
+	    :annotations => [{
+		    "note" => "tolles licht",
+		    "x" => 10,
+		    "y" => 10,
+		    "h" => 20,
+		    "w" => 30
+	    },
+	    {
+		    "note" => "wow",
+		    "x" => 50,
+	      "y" => 10,
+	      "h" => 40, 
+	      "w" => 10
+	    }],
+	    :thumb => "images/photo2.jpg"
+    },{
+      :id => 83,
+	    :exif => {
+		    :date => "today",
+		    :author => "",
+		    :model => "",
+		    :bla => ""
+		  },
+	    :iptc => {
+		    :stars => {
+			    :nr => 4,
+			    :of => 5
+		    },
+		    :tags => ["hasso", "plattner", "sommer", "absolventen feier", "#public"]
+	    },
+	    :url => "",
+	    :annotations => [{
+		    "note" => "tolles licht",
+		    "x" => 10,
+		    "y" => 10,
+		    "h" => 20,
+		    "w" => 30
+	    },
+	    {
+		    "note" => "wow",
+		    "x" => 50,
+	      "y" => 10,
+	      "h" => 40, 
+	      "w" => 10
+	    }],
+	    :thumb => "images/photo3.jpg"
+    },{
+      :id => 97,
+	    :exif => {
+		    :date => "today",
+		    :author => "",
+		    :model => "",
+		    :bla => ""
+		  },
+	    :iptc => {
+		    :stars => {
+			    :nr => 4,
+			    :of => 5
+		    },
+		    :tags => ["hasso", "plattner", "sommer", "absolventen feier", "#public"]
+	    },
+	    :url => "",
+	    :annotations => [{
+		    "note" => "tolles licht",
+		    "x" => 10,
+		    "y" => 10,
+		    "h" => 20,
+		    "w" => 30
+	    },
+	    {
+		    "note" => "wow",
+		    "x" => 50,
+	      "y" => 10,
+	      "h" => 40, 
+	      "w" => 10
+	    }],
+	    :thumb => "images/photo1.jpg"
     }]
     
     respond_to do |format|
