6b14bec8264c73e3e017cb5a7365b9fd1c782a83
Jan Graichen <jan.graichen@altimos.de>
2011-11-14 22:01:52 +0100

    Fix photo index column name.

--- db/migrate/20111114190319_add_file_index_to_photos.rb
+++ db/migrate/20111114190319_add_file_index_to_photos.rb
@@ -1,6 +1,6 @@
 class AddFileIndexToPhotos < ActiveRecord::Migration
   def change
     add_index :photos, :file
-    add_index :photos, [:file, :id], :unique => true
+    add_index :photos, [:file, :source_id], :unique => true
   end
 end
