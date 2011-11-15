e7f098cc756390a7b71a41f4909334b6cf9bfc97
Jan Graichen <jan.graichen@altimos.de>
2011-11-15 20:15:32 +0100

    Remove json formatting from model and deliver image thru rails.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,17 +1,21 @@
 class PhotosController < ApplicationController
   
   def index
-    @photos = Photo.all
+    @photos = PhotoDecorator.all
     
     respond_to do |format|
       format.html
       format.json do
-        render :json => @photos.as_json(:include => {
-            :tags => {}, 
-            :properties => {:include => :key},
-            :source => {}
-          })
+        render :json => @photos.as_json
       end
     end
+  end
+  
+  def show
+    @photo = Photo.find(params[:id])
+    
+    render :status => :not_found and return unless @photo.file =~ /\.#{params[:format]}$/
+    
+    send_file @photo.file
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,14 +3,5 @@ class Photo < ActiveRecord::Base
   belongs_to :source
   has_many :properties
   has_and_belongs_to_many :tags, :uniq => true
-  
-  
-  def as_json(opts)
-    {
-      :id => id,
-      :file => file,
-      :properties => Hash[self.properties.map { |p| [p.key.name, p.value] }],
-      :tags => self.tags.map(&:name)
-    }
-  end
+
 end
