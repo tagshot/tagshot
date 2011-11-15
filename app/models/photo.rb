9f9216d85fa625e14d18a1c6c5628245a7b09c64
Jan Graichen <jan.graichen@altimos.de>
2011-11-15 19:50:22 +0100

    Update photos json output and fix tag assoziation.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,4 +4,13 @@ class Photo < ActiveRecord::Base
   has_many :properties
   has_and_belongs_to_many :tags, :uniq => true
   
+  
+  def as_json(opts)
+    {
+      :id => id,
+      :file => file,
+      :properties => Hash[self.properties.map { |p| [p.key.name, p.value] }],
+      :tags => self.tags.map(&:name)
+    }
+  end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ActiveRecord::Base
 
-  has_and_belongs_to_many :photos
+  has_and_belongs_to_many :photos, :uniq => true
   
   validates_uniqueness_of :name
   validates_presence_of :name
