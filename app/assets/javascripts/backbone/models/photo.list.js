a549bc0d2b98854044de0867587506ffd46a2df7
dominik <domoritz@gmail.com>
2011-11-18 18:53:55 +0100

    bessere/ sauberere selektion

--- app/assets/javascripts/backbone/models/photo.list.js
+++ app/assets/javascripts/backbone/models/photo.list.js
@@ -1,9 +1,19 @@
+
 // array von photos
 Tagshot.Collections.PhotoList  = Backbone.Collection.extend({
 	model: Tagshot.Models.Photo,
 	url: "/photos",
+	intialize: function(){
+		_.bindAll(this, 'selectAll', 'deselectAll');
+	},
 	// return the current selection
 	selection: function() {
 		return this.filter(function(photo){ return photo.get('selected'); });
+	},
+	selectAll: function() {
+		_.map(this.models, function(item) { item.set({"selected": true}) });
+	},
+	deselectAll: function()	{
+		_.map(this.models, function(item) { item.set({"selected": false}) });	
 	}
 });
