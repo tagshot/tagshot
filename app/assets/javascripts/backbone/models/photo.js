383e68ee74cac94b6298c88bb2a1406f5ac4296c
dominik <domoritz@gmail.com>
2011-11-25 15:24:33 +0100

    neues fehlerhandling, welches extern ist

--- app/assets/javascripts/backbone/models/photo.js
+++ app/assets/javascripts/backbone/models/photo.js
@@ -1,20 +1,4 @@
-Tagshot.Models.ErrorHandlingModel = Backbone.Model.extend({
-	initialize: function(attributes, options) {
-		options || (options = {});
-		this.bind("error", this.defaultErrorHandler);
-		this.init && this.init(attributes, options);
-	},
-	defaultErrorHandler: function(model, error) {
-		console.error(error.statusText,error);
-		if (error.status == 401 || error.status == 403) {
-			// trigger event or route to login here.
-		} else {
-			alert("Status code: " + error.status + "\n" + error.statusText);
-		}
-	}
-});
-
-Tagshot.Models.Photo = Tagshot.Models.ErrorHandlingModel.extend({
+Tagshot.Models.Photo = Backbone.Model.extend({
 	initialize: function(options) {
 		Tagshot.Models.ErrorHandlingModel.prototype.initialize.call(this, options);
 	},
@@ -23,9 +7,6 @@ Tagshot.Models.Photo = Tagshot.Models.ErrorHandlingModel.extend({
 		return {
 			id: 0
 		};
-	},
-	star: function(nr, of) {
-		// TODO
 	},
 	isSelected: function() {
 		return this.selected;
