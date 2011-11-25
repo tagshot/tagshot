30efa1c55cfc9083cfecc7d919267b9b4451eb22
Stefan Bunk <stefan.bunk@googlemail.com>
2011-11-25 16:21:52 +0100

    MainView hinzugefuegt.

---
+++ app/assets/javascripts/backbone/views/main.view.js
@@ -0,0 +1,20 @@
+//= require backbone-eventdata
+
+Tagshot.Views.MainView = Backbone.View.extend({
+	el:  document,
+	events: {
+		"keydown[ctrl+a]" : "selectAll",
+		"keydown[meta+a]" : "selectAll",
+	},
+	selectAll: function() {
+		alert("select all");
+	},
+	initialize : function() {
+		Tagshot.collections.photoList = new Tagshot.Collections.PhotoList();
+		Tagshot.views.gallery = new Tagshot.Views.PhotoList({ collection: Tagshot.collections.photoList});
+		Tagshot.views.ajaxError = new Tagshot.Views.AjaxError();
+	},
+	render: function () {
+		$("#backbone-main-view").append(Tagshot.views.gallery.render().el);
+	}
+});
