fcf815bc5c84b135c71b64b99a0ba7c5c6ae2ba5
Stefan Bunk <stefan.bunk@googlemail.com>
2011-11-25 16:40:40 +0100

    main.view.js nach oben verschoben.

--- app/assets/javascripts/backbone/main.view.js
+++ app/assets/javascripts/backbone/main.view.js
@@ -1,3 +1,11 @@
+/*
+ *= require_self
+ *= require_tree ./initializers
+ *= require_tree ./templates
+ *= require_tree ./models
+ *= require_tree ./views
+ *= require_tree ./routers
+ */
 //= require backbone-eventdata
 
 Tagshot.Views.MainView = Backbone.View.extend({
