bbdfab03004fa4c2fb3390c5c5b5c83ea68f6138
Jan Graichen <jan.graichen@altimos.de>
2011-11-29 16:20:38 +0100

    Return full thumb url in photo json.

--- app/decorators/photo_decorator.rb
+++ app/decorators/photo_decorator.rb
@@ -4,7 +4,7 @@ class PhotoDecorator < ApplicationDecorator
   def as_json(opts = {})
     {
       :id => model.id,
-      :thumb => "photos/#{model.id}#{File.extname(model.file)}",
+      :thumb => h.photo_url(model) + File.extname(model.file),
       :meta => Hash[model.properties.map { |p| [p.name, p.value] }],
       :tags => model.tags.names,
       :properties => Hash[model.class.meta_property_names.map { |name| [name, model.send(name)] }]
