61b5eec76e3bf089492d45a27fe05fad7cf74ded
Willi <willi@jups42.de>
2012-01-18 15:21:15 +0100

    Fixing bug in loading/requireing of Backbone stuff

--- app/assets/javascripts/backbone/tagshot.js
+++ app/assets/javascripts/backbone/tagshot.js
@@ -1,10 +1,6 @@
 /*
  *= require_self
- *= require_tree ./initializers
- *= require_tree ./templates
- *= require_tree ./models
- *= require_tree ./views
- *= require_tree ./routers
+ *= require_tree .
  */
 
 window.Tagshot = {
