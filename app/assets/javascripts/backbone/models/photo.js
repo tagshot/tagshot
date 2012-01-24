f0d59db6849c6afd6a0a6ea961ce1e250e1f143f
Willi <willi@jups42.de>
2012-01-24 22:59:53 +0100

    added a little documentation to photo model

--- app/assets/javascripts/backbone/models/photo.js
+++ app/assets/javascripts/backbone/models/photo.js
@@ -1,4 +1,7 @@
-/* Here be documentation
+/* This model describes a photo
+ * The select/deselect/isSelected stuff helps to select multiple
+ * photos for batch processing in the photo.list.view.
+ * One can add tags to all or delete all the selected photos.
  */
 
 Tagshot.Models.Photo = Backbone.Model.extend({
@@ -6,32 +9,37 @@ Tagshot.Models.Photo = Backbone.Model.extend({
 	initialize: function() {
 		//_.bindAll(this, 'order', 'isSelected');
 	},
+
 	order: function() {
 		return this.id;
 	},
+
 	selected: false,
 	defaults: function() {
 		return {
 			id: 0
 		};
 	},
+
 	isSelected: function() {
 		return this.selected;
 	}(),
+
 	select: function() {
 		if (!this.selected) {
 			this.selected = true;
 			this.trigger("select");
 		}
 	},
+
 	deselect: function() {
 		if (this.selected) {
 			this.selected = false;
 			this.trigger("deselect");
 		}
 	},
+
 	toggleSelect: function() {
 		this.selected? this.deselect() : this.select();	
 	}
-
 });
