ead34000e44535be5042139a61cc0ee63fc045a9
Jan Graichen <jan.graichen@altimos.de>
2012-01-25 19:45:52 +0100

    Improve properties controller and specs.

--- spec/controllers/properties_controller_spec.rb
+++ spec/controllers/properties_controller_spec.rb
@@ -2,11 +2,32 @@ require 'spec_helper'
 
 describe PropertiesController do
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+  describe "GET index" do
+    context "as JSON" do
+      before(:each) { @photo = Factory(:photo_with_properties) }
+
+      it 'should require authentication' do
+        get :index, format: :json, photo_id: @photo.id
+        response.status.should == 302
+      end
+
+      context 'with authenticated user' do
+        before(:each) { set_current_user Factory(:user) }
+        
+        it 'should respond with OK' do
+          get :index, format: :json, photo_id: @photo.id
+          response.status.should == 200
+        end
+
+        it 'should respond hash of photo meta data' do
+          get :index, format: :json, photo_id: @photo.id
+
+          JSON(response.body).should == [
+            {'name' => 'Meta1', 'value' => 'Value1'},
+            {'name' => 'Meta2', 'value' => 'Value2'}
+          ]
+        end
+      end
     end
   end
-
 end
