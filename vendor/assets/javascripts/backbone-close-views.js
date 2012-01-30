03c366b07de56652a73f9d09033412996df79378
Willi <willi@jups42.de>
2012-01-30 16:50:39 +0100

    Documented close-views event pruner module

--- vendor/assets/javascripts/backbone-close-views.js
+++ vendor/assets/javascripts/backbone-close-views.js
@@ -1,3 +1,8 @@
+/* This module removes all existing event bindings when closing a view.
+ * It prevents dangling event bindings that may occur during navigation between views.
+ */
+
+
 Backbone.View.prototype.close = function(){
   this.remove();
   this.unbind();
