1828ccc554236f12b4cf98da4885ce9f0b3417c4
Jan Graichen <jan.graichen@altimos.de>
2012-02-02 08:44:29 +0100

    Categorize semantic meta data into readable and not readable. (refs #493)

--- app/decorators/photo_decorator.rb
+++ app/decorators/photo_decorator.rb
@@ -3,11 +3,12 @@ class PhotoDecorator < ApplicationDecorator
 
   def as_json(opts = {})
     {
-      :id => model.id,
-      :thumb => h.thumb_url(model, :format => model.extname.downcase),
-      :image => h.photo_url(model, :format => model.extname),
-      :tags => model.tags.names,
-      :properties => model.data.as_json
+      id: model.id,
+      thumb: h.thumb_url(model, :format => model.extname.downcase),
+      image: h.photo_url(model, :format => model.extname),
+      tags: model.tags.names,
+      meta: model.data.meta_properties(readonly: true).as_json,
+      properties: model.data.meta_properties(readonly: false).as_json
     }
   end
 end
