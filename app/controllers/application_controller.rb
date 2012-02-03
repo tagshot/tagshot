6108bf3c61af4b81ce3e94a46a7ad25d06d320ee
Jan Graichen <jan.graichen@altimos.de>
2012-02-03 13:33:41 +0100

    Fix require auth nil bug.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::Base
   def require_authentication
     unless User.current.logged?
       flash[:error] = 'Authentication required.'
-      if params[:format].downcase == 'html'
+      if params[:format].nil? or params[:format].to_s.downcase == 'html'
         redirect_to new_session_url
       else
         render_401
