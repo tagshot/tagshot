efd2df9ff9bce03eaf0092b040fa9b243a369e6f
Stefan Bunk <stefan.bunk@googlemail.com>
2012-02-09 13:41:19 +0100

    helpers code style.

--- app/assets/javascripts/helpers.js
+++ app/assets/javascripts/helpers.js
@@ -61,34 +61,34 @@ Tagshot.helpers = (function() {
 	var addGlobalAjaxIndicator = function () {
 		var indicator = $('#loading-image');
 
-		$("body").ajaxSend(function() {
-			indicator.stop(true,true).fadeIn(50);
+		$("body").ajaxSend(function () {
+			indicator.stop(true, true).fadeIn(50);
 		});
 
-		$("body").ajaxStop(function() {
+		$("body").ajaxStop(function () {
 			indicator.delay(500).fadeOut(100);
 		});
 	};
 
 
 	var message = function(message, time) {
-		Tagshot.helpers.genericMessage(message,time,false);
+		Tagshot.helpers.genericMessage(message, time, false);
 	};
 
 
 	var error = function(error, time) {
-		Tagshot.helpers.genericMessage(error,time,true);
+		Tagshot.helpers.genericMessage(error, time, true);
 	};
 
 
 	var genericMessage = function(message, time, alerted) {
-	var mb = $("#message-board");
-	if (alerted) {
-		mb.addClass("alerted");
-	} else {
-		mb.removeClass("alerted");
-	}
-	mb.html(message).stop(true, true).slideDown(200).delay(time).slideUp(100);
+		var mb = $("#message-board");
+		if (alerted) {
+			mb.addClass("alerted");
+		} else {
+			mb.removeClass("alerted");
+		}
+		mb.html(message).stop(true, true).slideDown(200).delay(time).slideUp(100);
 	};
 
 
