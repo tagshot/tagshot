7044c83327b020147a805b0b12f909a53187bd5e
Willi <willi@jups42.de>
2012-02-10 12:05:44 +0100

    require only necessary stuff in testrunner

--- app/assets/javascripts/tests/testrunner.js
+++ app/assets/javascripts/tests/testrunner.js
@@ -1,15 +1,10 @@
 // Here We require all files we need for testing
 
-//= require application
-//= require proglag
 //= require jquery
-//= require jquery_ujs
-//= require jquery.hotkeys
 //= require underscore
-//= require json2
 //= require backbone
-//= require backbone_rails_sync
-//= require backbone_datalink
+//= require mustache
+//= require backbone/tagshot
 
 //= require_tree ../tagshot
 
